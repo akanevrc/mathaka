455e1ac6ef8a5ad58e9135dd5ec2e66c01448a00
akanevrc <akanezoranomukou@gmail.com>
2024-02-17 18:03:04 +0900

    feat: mathakaコマンドの仮実装を追加

--- mathaka/src/main.rs
+++ mathaka/src/main.rs
@@ -1,3 +1,33 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(name = "mathaka", author, version, about, long_about = None)]
+struct Args {
+    /// Problem file paths
+    #[arg(short, long = "file")]
+    files: Vec<String>,
+}
+
+fn solve_problem_files(files: Vec<String>) -> Result<String> {
+    let mut props = Vec::new();
+    for file in files {
+        let prop = std::fs::read_to_string(file)?;
+        props.push(prop);
+    }
+    let mut res = true;
+    for prop in props {
+        res = res && mathakalib::solve_problems(prop)?;
+    }
+    if res {
+        Ok("TRUE - all propositions hold true.".to_owned())
+    } else {
+        Ok("FALSE - any propositions do not hold true.".to_owned())
+    }
+}
+
+fn main() -> Result<()> {
+    let args = Args::parse();
+    println!("{}", solve_problem_files(args.files)?);
+    Ok(())
 }
